80874a665b709e6502de05fa93acace37ed4f976
Ross Borchers <ross@renderheads.com>
2021-02-17 16:12:56 +0200

    Refactored folder structure and fixed some empty spaces in src

---
+++ Samples/Scripts/Modules/ISpawnerModule.cs
@@ -0,0 +1,39 @@
+﻿// (C) RenderHeads PTY LTD
+// Author: Shane Marks;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RenderHeads.Tooling.Core.ModulePattern.Sample
+{
+	public interface ISpawnerModule : IModule
+	{
+		/// <summary>
+		/// Return total amount of spawn objects since the start of the game
+		/// </summary>
+		/// <returns>Return total amount of spawn objects since the start of the game</returns>
+		int GetSpawnCount();
+
+		/// <summary>
+		/// Stop Spawning
+		/// </summary>
+		void Stop();
+
+
+		/// <summary>
+		/// Start spawning
+		/// </summary>
+		void Start();
+
+		/// <summary>
+		/// Returns a list of spawned objects
+		/// </summary>
+		/// <returns>Returns a list of spawned objects</returns>
+		List<GameObject> GetSpawnedObjects();
+
+		/// <summary>
+		/// Returns if the spawner is spawning
+		/// </summary>
+		/// <returns>Returns if the spawner is spawning</returns>
+		bool IsSpawning();
+	}
+}
---
+++ Samples/Scripts/Modules/SpawnerModule.cs
@@ -0,0 +1,88 @@
+﻿// (C) RenderHeads PTY LTD
+// Author: Shane Marks
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RenderHeads.Tooling.Core.ModulePattern.Sample
+{
+	public class ConsistentSpawnModule : ISpawnerModule
+	{
+		/// <summary>
+		/// Reference to gameobject to spawn
+		/// </summary>
+		private GameObject _toSpawn;
+		
+		/// <summary>
+		/// The last time a game object was spawned, will update when a new object to spawned
+		/// </summary>
+		private DateTime _lastSpawnTime;
+
+		/// <summary>
+		/// How long to wait between spawns
+		/// </summary>
+		private int _spawnDistanceInSeconds = 1;
+
+		/// <summary>
+		/// Will only spawn objects if this is true
+		/// </summary>
+		private bool _canSpawn = false;
+
+		/// <summary>
+		/// A list of spawned objects
+		/// </summary>
+		private List<GameObject> _spawnedObjects;
+
+		/// <summary>
+		/// Constructor for our module
+		/// </summary>
+		/// <param name="toSpawn">The object to spawn</param>
+		/// <param name="spawnImmediately">If true set canSpawn = true so it starts spawning as soon as its constructred</param>
+		public ConsistentSpawnModule(GameObject toSpawn, bool spawnImmediately)
+		{
+			_toSpawn = toSpawn;
+			_lastSpawnTime = DateTime.Now;
+			_spawnedObjects = new List<GameObject>();
+			_canSpawn = spawnImmediately;
+		}
+
+		public int GetSpawnCount()
+		{
+			return _spawnedObjects.Count;
+		}
+
+		public List<GameObject> GetSpawnedObjects()
+		{
+			return _spawnedObjects;
+		}
+
+		public bool IsSpawning()
+		{
+			return _canSpawn;
+		}
+
+		public void Start()
+		{
+			_canSpawn = true;
+			_lastSpawnTime = DateTime.Now;
+		}
+
+		public void Stop()
+		{
+			_canSpawn = false;
+		}
+
+		public void UpdateModule()
+		{
+			if (!_canSpawn)
+			{
+				return;
+			}
+			if (_lastSpawnTime.AddSeconds(_spawnDistanceInSeconds) < DateTime.Now)
+			{
+				_spawnedObjects.Add(GameObject.Instantiate(_toSpawn, new Vector3(UnityEngine.Random.Range(-10, 10), UnityEngine.Random.Range(-10, 10), UnityEngine.Random.Range(-10, 10)), Quaternion.identity));
+				_lastSpawnTime = DateTime.Now;
+			}
+		}
+	}
+}
